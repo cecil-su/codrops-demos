3d8dbb57aa1ebf6f0ab387ec0af29c96b2651241
cecil-su <cecil_su@163.com>
2017-06-29 16:41:21 +0800

    add line menu styles demo

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -5,6 +5,7 @@ import Hello from '@/components/Hello'
 const folderPreviewIdeas = r => require.ensure([], () => r(require('./folder-preview-ideas/index.vue')))
 const tooltipAnimations = r => require.ensure([], () => r(require('./tooltip-animations/index.vue')))
 const animocons = r => require.ensure([], () => r(require('./animocons/index.vue')))
+const lineMenuStyles = r => require.ensure([], () => r(require('./line-menu-styles/index.vue')))
 
 Vue.use(Router)
 
@@ -26,6 +27,10 @@ export default new Router({
     {
       path: '/Animocons',
       component: animocons
+    },
+    {
+      path: '/LineMenuStyles',
+      component: lineMenuStyles
     }
   ]
 })
---
+++ vue/src/router/line-menu-styles/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <svg class="hidden"></svg>
+    <main></main>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="stylus"></style>
